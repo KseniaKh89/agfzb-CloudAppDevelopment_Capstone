3c622840f61446f92416bfdc6e0357e81ccc9557
KseniaKh89 <ksenia.kharchenko89@gmail.com>
2022-11-23 06:03:11 -0500

    Adding temporary changes to Github

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -15,19 +15,29 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
-
-
 # Create an `about` view to render a static about page
-# def about(request):
-# ...
-
+def about(request):
+    return render(request,'djangoapp/about.html')
 
 # Create a `contact` view to return a static contact page
-#def contact(request):
+def contact(request):
+    return render(request,'djangoapp/contact.html')
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['psw']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            return render(request, 'djangoapp/index.html', context)
+    else:
+        return render(request, 'djangoapp/index.html', context)
+
 
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
